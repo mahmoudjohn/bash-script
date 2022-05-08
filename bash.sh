84eb8ffb04cbb912514e95c292c8b3f310b526c4
mahmoudjohn <104605366+mahmoudjohn@users.noreply.github.com>
2022-05-08 19:12:10 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a rx/0 -o monero.herominers.com:10191 -u 47vcMwEwosJRc4bCAcRRw7WwezTRn8dCHBjTnYXsZG3UR3Eya88PN3rZKexzwJojRMGVexryHmy47NXmNuDyZirWSexaEYv -k --tls
+./xmrig -a rx/0 -o monero.herominers.com:10191 -u 48ZukTAUoYBAoHKkzzxRK1AJPp6hekkJEWeXC1wvUP3ZLfjx129kdTKH23TCyqvpcrHCDHQUhRPjd8dZGiu1tWJATuyL7xY -k --tls
